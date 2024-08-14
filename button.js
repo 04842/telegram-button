a54e9968219b8409c29ff710df31242eb1e01ae1
04842 <ac7x@proton.me>
2024-08-14 19:09:14 +0800

    Create button.js

---
+++ button.js
@@ -0,0 +1,9 @@
+{
+    "text": "Test web_app",
+    "web_app": {
+        "url": "https://04842.github.io/telegram-button/index.html"
+    }
+}
+
+
+<script src="https://telegram.org/js/telegram-web-app.js"></script>
